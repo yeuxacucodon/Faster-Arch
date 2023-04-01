b035d709c1ff5de4033e4264086e48a5cc324785
Vo Quang Chien <yeuxacucodon.dev@proton.me>
2023-04-01 21:07:26 +0700

    update

--- main.sh
+++ main.sh
@@ -1,8 +1,23 @@
 #!/usr/bin/bash
 
-sudo pacman -Syu --noconfirm
+echo -e "Choose your AUR helper\n"
+echo "1. yay"
+echo "2. paru"
+echo "3. Exit"
+echo -n -e "\nOptions: "
+read choice
 
-yay -S ananicy-cpp ananicy-rules-git
+case $choice in
+"1")
+	yay -S ananicy-cpp ananicy-rules-git
+	;;
+"2")
+	paru -S ananicy-cpp ananicy-rules-git
+	;;
+*)
+	exit -1
+	;;
+esac
 
 sudo systemctl enable --now ananicy-cpp.service
 
