057b8550fe86eac95ffff64a90feca3d22297028
Vo Quang Chien <yeuxacucodon.dev@proton.me>
2023-04-22 07:55:31 +0700

    update

--- main.sh
+++ main.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/bash
 
-echo -e "Choose your AUR helper\n"
+echo -e "\033[1;31mUpdating your system...\n\033[0m"
+sudo pacman -Syu --noconfirm
+
+echo -e "\n\033[1;37mChoose your AUR helper:\n\033[0m"
 echo "1. yay"
 echo "2. paru"
 echo "3. Exit"
@@ -9,10 +12,10 @@ read choice
 
 case $choice in
 "1")
-	yay -S ananicy-cpp ananicy-rules-git
+	yay -S ananicy-cpp ananicy-rules-git --noconfirm
 	;;
 "2")
-	paru -S ananicy-cpp ananicy-rules-git
+	paru -S ananicy-cpp ananicy-rules-git --noconfirm
 	;;
 *)
 	exit -1
@@ -21,4 +24,6 @@ esac
 
 sudo systemctl enable --now ananicy-cpp.service
 
-systemctl status ananicy-cpp
+sudo pacman -S earlyoom --noconfirm
+
+sudo systemctl enable --now earlyoom
