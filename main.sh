c0018fc74d08e744fa5fb7d21ac5a1897391fbaf
Vo Quang Chien <yeuxacucodon.dev@proton.me>
2023-04-22 07:59:27 +0700

    update

--- main.sh
+++ main.sh
@@ -1,7 +1,10 @@
 #!/usr/bin/bash
 
 echo -e "\033[1;31mUpdating your system...\n\033[0m"
+
 sudo pacman -Syu --noconfirm
+
+echo -e "\n\033[0;32m----------------------------------\033[0m"
 
 echo -e "\n\033[1;37mChoose your AUR helper:\n\033[0m"
 echo "1. yay"
