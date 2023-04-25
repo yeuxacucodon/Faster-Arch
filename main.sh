c978fb2fd4d0dc134b79ffd8f7a96107a8bebd99
Vo Quang Chien <yeuxacucodon.dev@proton.me>
2023-04-25 14:16:22 +0700

    update

--- main.sh
+++ main.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/bash
 
+# Upgrade system
 echo -e "\033[1;31mUpdating your system...\n\033[0m"
-
 sudo pacman -Syu --noconfirm
 
+# Choose AUR helper
 echo -e "\n\033[0;32m----------------------------------\033[0m"
-
 echo -e "\n\033[1;37mChoose your AUR helper:\n\033[0m"
 echo "1. yay"
 echo "2. paru"
@@ -25,21 +25,23 @@ case $choice in
 	;;
 esac
 
-# Install linux-zen kernel
+# Install linux-zen kernel and update GRUB
 sudo pacman -S linux-zen --noconfirm
 if [[ $(pacman -Q grub | cut -f 1 -d " ") == "grub" ]]; then
 	echo -e "\n\033[1;31mUpdate GRUB...\n\033[0m"
 	sudo grub-mkconfig -o /boot/grub/grub.cfg
 fi
 
-# Use dbus-broker
+# Install dbus-broker
 sudo pacman -S dbus-broker --noconfirm
 sudo systemctl enable --now dbus-broker.service
 systemctl --user enable dbus-broker.service
 
+# Enable preload and ananicy-cpp
 sudo systemctl enable --now preload
 sudo systemctl enable --now ananicy-cpp.service
 
+# Install earlyoom
 sudo pacman -S earlyoom --noconfirm
 sudo systemctl enable --now earlyoom
 
